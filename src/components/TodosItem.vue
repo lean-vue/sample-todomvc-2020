60dbaa42b131a1c32fb82c8045450776682626f0
Michael Alt <mail@lean-stack.de>
2020-09-22 21:37:42 +0200

    Simplify the usage of actions.

--- src/components/TodosItem.vue
+++ src/components/TodosItem.vue
@@ -2,26 +2,22 @@
   <!-- List items should get the class `editing` when editing -->
   <li :class="{ completed: todo.completed }">
     <div class="view">
-      <input class="toggle" type="checkbox" :checked="todo.completed" @change="toggle">
+      <input class="toggle" type="checkbox" :checked="todo.completed" @change="toggle(todo.id)">
       <label>{{ todo.title }}</label>
-      <button class="destroy" @click="destroy"></button>
+      <button class="destroy" @click="destroy(todo.id)"></button>
     </div>
     <input class="edit" value="Create a TodoMVC template">
   </li>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 
 export default {
   name: "TodosItem",
   props: ['todo'],
   methods: {
-    toggle() {
-      this.$store.dispatch('todos/toggle', this.todo.id);
-    },
-    destroy() {
-      this.$store.dispatch('todos/destroy', this.todo.id);
-    }
+    ...mapActions('todos', ['toggle', 'destroy'])
   }
 }
 </script>
